1fb29188071eb8668b18ac6a64cb581a4db8a1b9
Steven Dev <steven@email.com>
2021-09-22 19:18:03 -0500

    19:17

--- src/connectDB.js
+++ src/connectDB.js
@@ -7,50 +7,49 @@ const conect_db = async () => {
   const MONGO_DB = "IngenioApi";
 
   //       `mongodb://admin:admin123@167.99.145.171:27017/HomePage?authSource=admin`,
-  // try {
-  //   mongoose
-  //     .connect(
-  //       `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`,
-  //       {
-  //         useNewUrlParser: true,
-  //       }
-  //     )
-  //     .then((db) => console.log(`conectado con exito `))
-  //     .catch((err) => console.log(err));
-  // } catch (err) {
-  //   console.log(err);
-  // }
-  // mongodb://admin:admin123@167.99.145.171:27017/IngenioApi?authSource=admin
   try {
     mongoose
-      .connect("mongodb://localhost/ingenio_app_Lenguages", {
-        useNewUrlParser: true,
-      })
+      .connect(
+        `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`,
+        {
+          useNewUrlParser: true,
+        }
+      )
       .then((db) => console.log(`conectado con exito `))
       .catch((err) => console.log(err));
   } catch (err) {
     console.log(err);
   }
+  // mongodb://admin:admin123@167.99.145.171:27017/IngenioApi?authSource=admin
+  //   try {
+  //     mongoose
+  //       .connect("mongodb://localhost/ingenio_app_Lenguages", {
+  //         useNewUrlParser: true,
+  //       })
+  //       .then((db) => console.log(`conectado con exito `))
+  //       .catch((err) => console.log(err));
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  // const MONGO_USERNAME = 'admin';
+  // const MONGO_PASSWORD = "admin123";
+  // const MONGO_HOSTNAME = '127.0.0.1';
+  // const MONGO_PORT = '27017';
+  // const MONGO_DB = 'IngenioApi';
+
+  // try {
+  //      mongoose.connect(`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`,{
+  //         useNewUrlParser: true
+  //     })
+  //         .then(db =>  console.log(`conectado con exito `))
+  //         .catch(err =>  console.log('hubo un error', err))
+
+  // }catch(err){
+  //     console.log(err)
+  // }
 };
-
-// const MONGO_USERNAME = 'admin';
-// const MONGO_PASSWORD = "admin123";
-// const MONGO_HOSTNAME = '127.0.0.1';
-// const MONGO_PORT = '27017';
-// const MONGO_DB = 'IngenioApi';
-
-// try {
-//      mongoose.connect(`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`,{
-//         useNewUrlParser: true
-//     })
-//         .then(db =>  console.log(`conectado con exito `))
-//         .catch(err =>  console.log('hubo un error', err))
-
-// }catch(err){
-//     console.log(err)
-// }
-
-// }
 module.exports = {
   conect_db,
 };
